9496c89c5ed9e9cd5e84be6650918566d7d96108
anomalia <luiza.llocatelli@gmail.com>
2026-05-26 10:38:41 -0300

    Create Pessoa class with getters and setters
    Implement Pessoa class with attributes and methods for name and age management.

---
+++ aula 4/exercicio2.py
@@ -0,0 +1,33 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.__nome = nome
+        self.__idade = idade
+
+    def get_nome(self):
+        return self.__nome
+
+    def set_nome(self, nome):
+        if nome != "":
+            self.__nome = nome
+        else:
+            print("Nome inválido")
+
+    def get_idade(self):
+        return self.__idade
+
+    def set_idade(self, idade):
+        if 0 <= idade <= 120:
+            self.__idade = idade
+        else:
+            print("Idade inválida")
+          
+    def apresentar(self):
+        print("Nome:", self.__nome)
+        print("Idade:", self.__idade)
+
+pessoa = Pessoa("Guilherme", 17)
+
+pessoa.apresentar()
+
+pessoa.set_idade(130)
+pessoa.set_nome("")
